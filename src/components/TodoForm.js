50192ff0e241c7c14a648545d60867deb7aa16ac
Simon Zhang <zhangshi0512@gmail.com>
2023-03-10 15:00:52 -0800

    react todo list first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page from reload
+
+    addTodo(value);
+
+    setValue("");
+  };
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="enter your task today"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
